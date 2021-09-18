6396ced7248596284eb971831267317269551be3
123 <1171118793@qq.com>
2021-09-18 14:06:26 +0800

    修改后

--- exel.py
+++ exel.py
@@ -3,68 +3,62 @@ import xlrd
 wb = xlrd.open_workbook(r"C:\Users\11711\Desktop\python基础课\day7\2020年每个月的销售情况.xlsx")
 
 class shop (object):
-    def all_sum (self):            # 年销售总额
+    def all_sum (self,start=0, stop=12):            # 【start，stop）表中的销售总额
         sum = 0
-        for i in range(0,12):
+        for i in range(start,stop):
             # 确定选项卡
             sheet = wb.sheet_by_index(i)
             # 读取行与列
             row = sheet.nrows   # 行
             # col = sheet.ncols   # 列
 
-            for j in range(1,row):
+            for j in range(1,row):               # 累加销售额
                 data = sheet.row_values(j)
                 sum =sum + data[2]*data[4]
-        return sum
+        return sum                               # 返回销售额
 
     def species(self,start=0,stop=12):   #  获取衣服种类
         name = []
-        for i in range(start,stop):
+        for i in range(start,stop):      # 获取【start，stop）表中的所有服装名称，存到neme列表中
             sheet = wb.sheet_by_index(i)
             row = sheet.nrows
             for j in range(1,row):
                 data = sheet.row_values(j)
                 name.append(data[1])
-        name=set(name)
+        name=set(name)                 # 对列表name进行去重处理
         name=list(name)
-        return name
+        return name                    # 返回服装名称列表
 
-    def zhanbi(self,start=0,stop=12):           # 占比
+    def zhanbi(self,start=0,stop=12):           # 占比分析方法，参数“start=0,stop=12”默认是全年的分析，可具体到某某月的分析
         name = shop.species(self,start=start,stop=stop)               # 获取衣服种类
-        num = [0]*len(name)                     # 存储对应衣服的数量
-        money = [0]*len(name)                   # 存储对应衣服的销售金额
-        sum_num = 0
-        sum_money = shop.all_sum(self)
+        num = [0]*len(name)                     # 销售数据列表，存储对应衣服的数量且与服装名称对应关系
+        money = [0]*len(name)                   # 单种服装销售金额列表，存储与服装名称对应的销售金额
+        sum_num = 0                             # 总销售数量
+        sum_money = shop.all_sum(self,start=start,stop=stop)          # 销售总金额
 
-        for i in range(start,stop):
+        for i in range(start,stop):             # 根据参数start、stop获取【start，stop）表信息，获取表数据
             sheet = wb.sheet_by_index(i)
             row = sheet.nrows
 
             for j in range(1,row):
                 data = sheet.row_values(j)
-                sum_num = sum_num + data[4]
+                sum_num = sum_num + data[4]     # 累计销售总数量
 
                 for x in range(len(name)):
                     if data[1]==name[x]:
-                        num[x]=num[x]+data[4]
-                        money[x]=money[0]+data[2]*data[4]
-
-        # for y in range(len(name)):
-        #     print("%s今年卖%s件，收入%s元"% (name[y], int(num[y]), round(money[y],2)))
-        #     print("销售（件数）占:%s  销售（金额）占：%s"% ('{:.2%}'.format(num[y]/sum), '{:.2%}'.format(money[y]/sum1)))
-        #     print("")
-
-        return name, num, money ,sum_num ,sum_money
+                        num[x]=num[x]+data[4]  # 累计单种服装销售数量
+                        money[x]=money[0]+data[2]*data[4]  # 累计单种服装的销售金额
+        return name, num, money ,sum_num ,sum_money                # 返回服装名称列表、销售数量列表、销售额列表、总销售量、总销售金额
 
     def yue_zhanbi(self):              # 每个月每个类型衣服的月销售额占比
         for month in range(0,12):
-            name,num,money,sum_num,sum_moeny = shop.zhanbi(self,start=month ,stop=month+1 )
+            name,num,money,sum_num,sum_moeny = shop.zhanbi(self,start=month ,stop=month+1 )    # 调用占比分析方法
             for i in range(len(name)):
                 print("%s卖了%s元，销售额占%s月份总销售额的%s"% (name[i], round(money[i], 2), month+1, '{:.2%}'.format(money[i]/sum_moeny)))
             print("")
         return 0
 
-    def max(self,quarter=5,moder="max"):
+    def max(self,quarter=5,moder="max"):        # 季度、年度最畅销服装分析
         if moder=="max":
             if quarter == 1:  # 第一季度 2、3、4
                 name ,num ,money,sum,sum1 = shop.zhanbi(self,start=2-1,stop=4)
@@ -73,11 +67,7 @@ class shop (object):
                 name, num, money, sum, sum1 = shop.zhanbi(self, start=5-1, stop=7)
 
             elif quarter == 3:  # 第三季度 8、9、10
-                name = shop.species(self)  # 获取衣服种类
-                num = [0] * len(name)  # 存储对应衣服的数量
-
-                for i in range(7, 10):
-                    name, num, money, sum, sum1 = shop.zhanbi(self, start=8-1, stop=10)
+                name, num, money, sum, sum1 = shop.zhanbi(self, start=8-1, stop=10)
 
             elif quarter == 4:  # 第四季度 11、12、1月份
                 name = []
@@ -111,7 +101,8 @@ class shop (object):
                 return 0
 
             max = 0
-            for i in range(len(num)):
+            for i in range(len(num)):    # 遍历的销售数量 《 下标为max的销售  max不变
+                                         # 否则 max = i ,将下标i赋给max,定位销售数量最多的服装
                 if num[max] >= num[i]:
                     max = max
                 else:
@@ -121,22 +112,11 @@ class shop (object):
             else:
                 print("第%s季度最畅销的衣服是%s,卖出了%s件" % (quarter, name[max], int(num[max])))
 
-        elif moder=="min":
-            name = shop.species(self)  # 获取衣服种类
-            num = [0] * len(name)  # 存储对应衣服的数量
-
-            for i in range(0, 12):
-                sheet = wb.sheet_by_index(i)
-                row = sheet.nrows
-
-                for j in range(1, row):
-                    data = sheet.row_values(j)
-
-                    for x in range(len(name)):
-                        if data[1] == name[x]:
-                            num[x] = num[x] + data[4]
+        elif moder=="min":                # 全年最不好卖的服装分析
+            name, num, money, sum, sum1 = shop.zhanbi(self)
             min = 0
-            for i in range(len(num)):
+            for i in range(len(num)):       # 遍历的销售数量 > 下标为min的销售  min不变
+                                            # 否则 min = i ,将下标i赋给min,定位销售数量最少的服装
                 if num[min] < num[i]:
                     min = min
                 else:
@@ -146,7 +126,8 @@ class shop (object):
         else:
             print("仅仅提供季度畅销查询和年销售最低查询。")
             return 0
-    def show(self):
+
+    def show(self):                 # 打印功能选项
         info = '''
 *************服装商城管理系统功能选项卡************
 *--------- 1、年销售额查询            ---------*
@@ -162,7 +143,7 @@ class shop (object):
 a = shop()
 a.show()
 while True:
-    choose = input("速度七十迈心情自由自在，请选择：")
+    choose = input("速度七十迈，心情自由自在，请选择：")
     if choose == "1":
         sum=a.all_sum()      # 年销售额查询
         print("年销售额%s"% round(sum,2))
@@ -178,19 +159,11 @@ while True:
         y = input("季度查询输入1，年度查询输入2 :")    # 年、季度最畅销衣服查询和年最不景气的衣服查询
         if y.isdecimal():
             if y == "1":
-                # for x in range(1,5):
-                a.max(quarter=1)
-                a.max(quarter=2)
-                a.max(quarter=3)
-                a.max(quarter=4)
-                # x = input("自由自在的造，输入需要查询的季度：")
-                # if x.isdecimal() and (x=="1" or x=="2"or x=="3" or x=="4"):
-                #     x = int(x)
-                # else:
-                #     print("瞎输入，一边玩泥巴去。")
+                for x in range(1,5):
+                    a.max(quarter=x)
             elif y == "2":
-                a.max()
-                a.max(moder="min")
+                a.max()                 # 年度最畅销的服装
+                a.max(moder="min")      # 年度最不好卖的服装
             else:
                 print("瞎输入，一边玩泥巴去。")
         else:
@@ -203,4 +176,3 @@ while True:
         break
     else:
         print("今天星期三，上班呢，别瞎搞。")
-
